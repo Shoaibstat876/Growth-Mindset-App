0aa300512e11e6831762f9be295dc84bee928553
Muhammad Shoaib <shoaibstar876@gmail.com>
2025-03-17 02:23:37 +0500

    First Streamlit Web App

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🚀 Growth Mindset Challenge Web App")
+st.write("Hello! This is your first AI-powered web app with Streamlit!")
+
+if st.button("Click Me!"):
+    st.success("Yay! Your first Streamlit app is working! 🎉")
